00679048c087a5c02d08b7268f45247746428290
maaonan <xiaozhiqiu13@sina.com>
2018-09-18 15:48:51 +0800

    Fuck

---
+++ app/src/main/java/reagodjj/example/com/exercise/ui/fragment/TextFragment.java
@@ -0,0 +1,19 @@
+package reagodjj.example.com.exercise.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import reagodjj.example.com.exercise.R;
+
+public class TextFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.new_fragment_first, container,false);
+    }
+}
