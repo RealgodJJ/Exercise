50cc22f9d9354f9315a2724b6fc9d4026c82c80a
maaonan <xiaozhiqiu13@sina.com>
2018-10-23 22:36:31 +0800

    Add SeekBar, ProgressBar and RatingBar.

--- app/src/main/java/reagodjj/example/com/exercise/ui/RadioButtonActivity.java
+++ app/src/main/java/reagodjj/example/com/exercise/ui/RadioButtonActivity.java
@@ -1,13 +1,18 @@
 package reagodjj.example.com.exercise.ui;
 
+import android.icu.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -20,6 +25,10 @@ public class RadioButtonActivity extends AppCompatActivity implements RadioGroup
     private ImageView ivHappy;
     private ToggleButton tbHappy;
     private SeekBar sbProcess;
+    private ProgressBar pbProcess;
+    private Button btMaimProcess, btSecondProcess;
+    private RatingBar rbRate;
+    private TextView tvRate;
     private static final String TAG = "RealgodJJ";
 
     @Override
@@ -27,13 +36,11 @@ public class RadioButtonActivity extends AppCompatActivity implements RadioGroup
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio_button);
 
-        rgSex = findViewById(R.id.rg_sex);
-        rbMale = findViewById(R.id.rb_male);
-        rbFemale = findViewById(R.id.rb_female);
-        ivHappy = findViewById(R.id.iv_happy);
-        tbHappy = findViewById(R.id.tb_happy);
-        sbProcess = findViewById(R.id.sb_process);
+        initView();
+        initListener();
+    }
 
+    private void initListener() {
         rgSex.setOnCheckedChangeListener(this);
         tbHappy.setOnClickListener(this);
         sbProcess.setMax(100);
@@ -54,6 +61,30 @@ public class RadioButtonActivity extends AppCompatActivity implements RadioGroup
                 Log.d(TAG, "onProgressChanged(Stop): " + seekBar.getProgress());
             }
         });
+        btMaimProcess.setOnClickListener(this);
+        btSecondProcess.setOnClickListener(this);
+        rbRate.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                DecimalFormat df = new DecimalFormat("0.0");
+                tvRate.setText(String.format(getResources().getString(R.string.rate_information),
+                        df.format(rating), df.format(ratingBar.getStepSize()), fromUser));
+            }
+        });
+    }
+
+    private void initView() {
+        rgSex = findViewById(R.id.rg_sex);
+        rbMale = findViewById(R.id.rb_male);
+        rbFemale = findViewById(R.id.rb_female);
+        ivHappy = findViewById(R.id.iv_happy);
+        tbHappy = findViewById(R.id.tb_happy);
+        sbProcess = findViewById(R.id.sb_process);
+        pbProcess = findViewById(R.id.pb_process);
+        btMaimProcess = findViewById(R.id.bt_main_process);
+        btSecondProcess = findViewById(R.id.bt_second_process);
+        rbRate = findViewById(R.id.rb_rate);
+        tvRate = findViewById(R.id.tv_rate);
     }
 
     @Override
@@ -79,10 +110,20 @@ public class RadioButtonActivity extends AppCompatActivity implements RadioGroup
             case R.id.tb_happy:
                 if (tbHappy.isChecked()) {
                     ivHappy.setImageResource(R.mipmap.happy);
+                    sbProcess.setThumb(getDrawable(R.mipmap.happy));
                 } else {
                     ivHappy.setImageResource(R.mipmap.unhappy);
+                    sbProcess.setThumb(getDrawable(R.mipmap.unhappy));
                 }
                 Toast.makeText(this, tbHappy.getText().toString(), Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.bt_main_process:
+                pbProcess.incrementProgressBy(10);
+                break;
+
+            case R.id.bt_second_process:
+                pbProcess.incrementSecondaryProgressBy(5);
                 break;
         }
     }
